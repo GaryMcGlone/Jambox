330188f100a9570b54a6e916671e654aab1bd655
Gary Mc Glone <S00171995@mail.itsligo.ie>
2018-11-02 22:00:06 +0000

    fixed cors errors temporarily using proxy - needs to be changed because it's slow

--- src/app/services/new-releases/new-releases.service.ts
+++ src/app/services/new-releases/new-releases.service.ts
@@ -10,18 +10,18 @@ import { Results } from "../../interfaces/new-release.interface";
 export class NewReleasesService {
   private newReleasesURL =
     "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.json";
+  
+   private cors = "https://cors-anywhere.herokuapp.com/";
 
   constructor(private http: HttpClient) {}
 
   getAlbums(): Observable<Results[]> {
-    let headers: HttpHeaders = new HttpHeaders();
-    headers.append("X-Requested-With","XMLHttpRequest");
-    headers.append("Access-Control-Allow-Credentials", "true" );
-    headers.append('Access-Control-Allow-Credentials', 'localhost:8100')
+    // let headers: HttpHeaders = new HttpHeaders();
+    // headers.append("X-Requested-With","XMLHttpRequest");
+    // headers.append("Access-Control-Allow-Credentials", "true" );
+    // headers.append('Access-Control-Allow-Credentials', 'localhost:8100')
     return this.http
-      .get<Results[]>(this.newReleasesURL, {
-        headers: headers
-      })
+      .get<Results[]>(this.cors+this.newReleasesURL)
       .pipe(tap(res => console.log(res)));
   }
 }
