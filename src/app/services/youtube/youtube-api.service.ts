ae7fee7d33e06f282b21c143958fee63e95b8848
s00172641 <s00172641@mail.itsligo.ie>
2018-11-05 12:57:48 +0000

    youtube api (only searches music)

--- src/app/services/youtube/youtube-api.service.ts
+++ src/app/services/youtube/youtube-api.service.ts
@@ -5,22 +5,29 @@ import { catchError, tap } from "rxjs/operators";
 
 @Injectable()
 export class YouTubeApiService {
+  //https://www.googleapis.com/youtube/v3/search?part=snippet&part=id
+  //&q=Nice
+  //&regionCode=US
+  //&type=video
+  //&videoCategoryId=10
 
-  private _searchSiteURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=';
+  // private _searchSiteURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=';
+  private _searchSiteURL = 'https://www.googleapis.com/youtube/v3/search?part=snippet&part=id&q=';
   private _nextPageURL = 'https://www.googleapis.com/youtube/v3/search?pageToken=';
   private _mostPopSiteURL = 'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular';
   private _param = '&key=';
   private _max = '&maxResults=';
   private _sort = '&order=';
   private _query = '&q=';
-  private _type = '&'
+  private _typeVid = '&type=video';
+  private _vidCategoryId = '&videoCategoryId=10';
   private _key = 'AIzaSyBwTqh7G-xV4WWZg_-QFB04K4vCPjLXzAY';
 
   constructor(private _http: HttpClient) { }
 
   getSearchedVideos(query, filter, limit): Observable<YouTubeResponseSearch> {
-    return this._http.get<YouTubeResponseSearch>(this._searchSiteURL + query + this._sort + filter 
-      + this._max + limit  + this._param + this._key)
+    return this._http.get<YouTubeResponseSearch>(this._searchSiteURL + query + this._typeVid + this._vidCategoryId +
+       this._sort + filter + this._max + limit  + this._param + this._key)
       .pipe(tap(data => console.log('Search Data:', data)),
       catchError(this.handleError));
   }
