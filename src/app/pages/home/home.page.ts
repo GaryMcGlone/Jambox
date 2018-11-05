69dec2593d04e5b69da082d51533957bba9b7fbe
s00172641 <s00172641@mail.itsligo.ie>
2018-11-05 16:07:33 +0000

    home.page.ts update

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FirebaseAuthService } from "../../services/firebaseAuth/firebase-auth.service";
-import { DatabaseService } from "../../services/database/database.service";
+
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
@@ -8,11 +7,8 @@ import { DatabaseService } from "../../services/database/database.service";
 })
 export class HomePage implements OnInit {
   constructor(
-    private auth: FirebaseAuthService,
-    private dbService: DatabaseService
   ) {}
 
   ngOnInit() {
-    this.auth.checkIfLoggedIn();
   }
 }
