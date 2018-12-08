cd1388d90ce9b8afe0212359c98ee22b48f5b679
conno <s00172979@mail.itsligo.ie>
2018-12-08 20:17:18 +0000

    production database added to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAQWBZQlqaR8_G1W2L2iIHXYjzJvDwdUxE",
+    authDomain: "jamboxproduction-327c8.firebaseapp.com",
+    databaseURL: "https://jamboxproduction-327c8.firebaseio.com",
+    projectId: "jamboxproduction-327c8",
+    storageBucket: "jamboxproduction-327c8.appspot.com",
+    messagingSenderId: "93284455017"
+  }
 };
