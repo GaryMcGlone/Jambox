8a98f91dec7ba0518a57f81c20ac92169183721d
Connor <s00172979@mail.itsligo.ie>
2019-02-17 14:27:21 +0000

    added profile list

---
+++ src/app/components/profile-post-list/profile-post-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-post-list',
+  templateUrl: './profile-post-list.component.html',
+  styleUrls: ['./profile-post-list.component.scss']
+})
+export class ProfilePostListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
