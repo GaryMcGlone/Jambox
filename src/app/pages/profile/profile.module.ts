f7b1eb3eaa44685f952e30a5ee90f1942851c985
Jakub Pawlak <s00172641@mail.itsligo.ie>
2019-02-27 20:50:50 +0000

    Added profile liked posts components/ segments to profile

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -8,6 +8,9 @@ import { IonicModule } from '@ionic/angular';
 import { ProfilePage } from './profile.page';
 import { ProfilePostListComponent } from '../../components/profile-post-list/profile-post-list.component';
 import { ProfilePostComponent } from '../../components/profile-post/profile-post.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { ProfileLikedPostListComponent } from '../../components/profile-liked-post-list/profile-liked-post-list.component';
+import { ProfileLikedPostComponent } from '../../components/profile-liked-post/profile-liked-post.component';
 
 const routes: Routes = [
   {
@@ -21,8 +24,15 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FontAwesomeModule
   ],
-  declarations: [ProfilePage,  ProfilePostListComponent, ProfilePostComponent]
+  declarations: [
+    ProfilePage,  
+    ProfilePostListComponent, 
+    ProfilePostComponent,
+    ProfileLikedPostListComponent,
+    ProfileLikedPostComponent  
+  ]
 })
 export class ProfilePageModule {}
