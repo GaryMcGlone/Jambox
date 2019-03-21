263ff35492dd74b7448ecf8ee0dd3249a82c5970
Gary <s00171995@mail.itsligo.ie>
2019-03-21 13:14:18 +0000

    moved configs to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,15 @@ export const environment = {
     projectId: "jamboxproduction-327c8",
     storageBucket: "jamboxproduction-327c8.appspot.com",
     messagingSenderId: "93284455017"
+  },
+  spotifyConfig: {
+    clientId: "6e9fbfb6b8994a4ab553758dc5e38b13",
+    redirectUrl: "jamboxapp://callback",
+    scopes: ["streaming", "playlist-read-private", "user-read-email", "user-read-private", "user-read-currently-playing", "user-read-birthdate"],
+    tokenExchangeUrl: "https://jambox-app.herokuapp.com/exchange",
+    tokenRefreshUrl: "https://jambox-app.herokuapp.com/refresh"
+  },
+  spotifyClientID: {
+    clientId: "6e9fbfb6b8994a4ab553758dc5e38b13"
   }
 };
